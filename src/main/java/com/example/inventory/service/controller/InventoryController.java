5d913aeb49645b07bab9c0b1ff53209b0614870a
vvodianytskyi <vvodianytskyi@griddynamics.com>
2022-04-05 15:33:03 +0300

    Add dependency management and add RequiredArgsConstructor to Controller

--- src/main/java/com/example/inventory/service/controller/InventoryController.java
+++ src/main/java/com/example/inventory/service/controller/InventoryController.java
@@ -3,8 +3,8 @@ package com.example.inventory.service.controller;
 import com.example.inventory.service.model.InventoryItem;
 import com.example.inventory.service.repository.InventoryItemRepository;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class InventoryController {
 
     private final InventoryItemRepository inventoryItemRepository;
 
-    @Autowired
-    public InventoryController(InventoryItemRepository inventoryItemRepository) {
-        this.inventoryItemRepository = inventoryItemRepository;
-    }
-
     @GetMapping("/api/inventory/{productCode}")
     public ResponseEntity<InventoryItem> findInventoryByProductCode(@PathVariable("productCode") String productCode) {
-        log.info("Finding inventory for product code :" + productCode);
+        log.info("Finding inventory for product code: " + productCode);
         Optional<InventoryItem> inventoryItem = inventoryItemRepository.findByProductCode(productCode);
         if (inventoryItem.isPresent()) {
             return new ResponseEntity(inventoryItem, HttpStatus.OK);
